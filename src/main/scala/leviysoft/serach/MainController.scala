96954736d8611191eb8eefbb4ea4d22a6568648e
Daniil Smirnov <danslapman@gmail.com>
2016-07-29 00:01:32 +0300

    Integrate Cousine font for code request code editing

--- src/main/scala/leviysoft/serach/MainController.scala
+++ src/main/scala/leviysoft/serach/MainController.scala
@@ -1,8 +1,20 @@
 package leviysoft.serach
 
+import scalafx.event.ActionEvent
+import scalafx.scene.control.{Button, TextArea}
 import scalafxml.core.macros.sfxml
 
 @sfxml
-class MainController {
+class MainController(
+  private val connectButton: Button,
+  private val executeButton: Button,
+  private val requestEditor: TextArea
+) {
+  def onConnect(ev: ActionEvent): Unit = {
+    println("Connect")
+  }
 
+  def onExecute(ev: ActionEvent): Unit = {
+    println("Execute")
+  }
 }
--- src/main/scala/leviysoft/serach/Serach.scala
+++ src/main/scala/leviysoft/serach/Serach.scala
@@ -3,16 +3,23 @@ package leviysoft.serach
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.scene.Scene
+import scalafx.scene.text.Font
 import scalafxml.core.{DependenciesByType, FXMLView}
 
 object Serach extends JFXApp {
+  Font.loadFont(getClass.getResource("/fonts/cousine/Cousine-Regular.ttf").toExternalForm, 12)
+
+  val mainScene = new Scene(
+    FXMLView(
+      getClass.getResource("main.fxml"),
+      new DependenciesByType(Map())
+    )
+  )
+
+  mainScene.stylesheets.add(getClass.getResource("main.css").toExternalForm)
+
   stage = new JFXApp.PrimaryStage() {
     title = "Serach [Alpha] for ElasticSearch 2.3.x"
-    scene = new Scene(
-      FXMLView(
-        getClass.getResource("main.fxml"),
-        new DependenciesByType(Map())
-      )
-    )
+    scene = mainScene
   }
 }
