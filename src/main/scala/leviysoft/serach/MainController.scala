e4c8ca07d5633ffd83a3d401e66bbfdf2915e290
Daniil Smirnov <danslapman@gmail.com>
2016-08-02 22:04:37 +0300

    Add index and type selectors

--- src/main/scala/leviysoft/serach/MainController.scala
+++ src/main/scala/leviysoft/serach/MainController.scala
@@ -1,13 +1,16 @@
 package leviysoft.serach
 
+import javafx.beans.value.ObservableValue
 import javafx.scene.control.Alert
 import javafx.scene.{input => jfxsi}
 
 import com.sksamuel.elastic4s.ElasticClient
 import leviysoft.serach.compiler.DSLCompiler
+import org.elasticsearch.action.admin.indices.mapping.get.GetMappingsRequest
+import play.api.libs.json.JsValue
 
 import scalafx.event.ActionEvent
-import scalafx.scene.control.{Button, TextArea, TextField}
+import scalafx.scene.control._
 import scalafx.scene.input.KeyEvent
 import scalafxml.core.macros.sfxml
 
@@ -15,8 +18,11 @@ import scalafxml.core.macros.sfxml
 class MainController(
   private val serverAddress: TextField,
   private val connectButton: Button,
+  private val indexSelector: ComboBox[String],
+  private val typeSelector: ComboBox[String],
   private val executeButton: Button,
-  private val requestEditor: TextArea
+  private val requestEditor: TextArea,
+  private val responseAst: TreeView[JsValue]
 ) {
   private var client: ElasticClient = _
 
@@ -30,9 +36,23 @@ class MainController(
 
   def onConnect(ev: ActionEvent): Unit = {
     client = ElasticClient.transport(s"elasticsearch://${serverAddress.text()}")
+    client.admin.cluster().prepareState().execute().actionGet().getState.getMetaData.concreteAllIndices().foreach { indexName =>
+      indexSelector += indexName
+    }
+    indexSelector.value.addListener((observable: ObservableValue[_ <: String], oldValue: String, newValue: String) => {
+      typeSelector.getItems.clear()
+      client.admin.indices().getMappings(new GetMappingsRequest().indices(newValue)).get().mappings().get(newValue).keysIt()
+        .forEachRemaining((t: String) => typeSelector += t)
+    })
+  }
+
+  def indexSelected(ev: ActionEvent): Unit = {
+
   }
 
   def onExecute(ev: ActionEvent): Unit = {
+    client.admin.indices()
+
     val query = DSLCompiler.compileQuery(requestEditor.text())
 
     val alert = new Alert(Alert.AlertType.INFORMATION)
